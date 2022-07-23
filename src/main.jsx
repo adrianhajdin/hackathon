ee315a3b58dfe150bd46dcf56c569d75fee850b2
adrianhajdin <adrianhajdin00@gmail.com>
2022-07-23 17:18:13 +0200

    add auth

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReactDOM from 'react-dom/client'
+import React from 'react'
+
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+    </Routes>
+  </BrowserRouter>
+)
